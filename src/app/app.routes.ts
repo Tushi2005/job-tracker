ef5ad84f193abd0e84425f831a37fda5cbe8b959
Tuska Adam <tuskaadam2005@gmail.com>
2026-04-03 13:35:49 +0200

    feat: add application-list component, implement authentication UI and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { ApplicationsList } from './features/applications/applications-list/applications-list';
 import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'applications', component: ApplicationsList, canActivate: [authGuard] },
   { path: '', redirectTo: '/applications', pathMatch: 'full' },
   { path: '**', redirectTo: '/applications' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './features/auth/login/login';
-import { Register } from './features/auth/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [ Register],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
