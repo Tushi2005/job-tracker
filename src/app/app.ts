88d83c5e6c59773dfbe66b6599c328c4ca28a086
Tuska Adam <tuskaadam2005@gmail.com>
2026-04-03 12:18:56 +0200

    feat: add registration component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [ Login],
+  imports: [ Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
