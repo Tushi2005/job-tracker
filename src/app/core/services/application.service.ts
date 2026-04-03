784829865579b3fa6aee6cf08a49d7a90bb8fc66
Tuska Adam <tuskaadam2005@gmail.com>
2026-04-03 10:41:33 +0200

    feat(core): add applicationService and authService

---
+++ src/app/core/services/application.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Application } from '../models/applications.model';
+
+@Injectable({providedIn:'root'})
+export class ApplicationService{
+    private apiUrl = 'https://localhost:7194/api/applications';
+
+    constructor(private http:HttpClient) {}
+
+    getAll(){
+        return this.http.get<Application[]>(this.apiUrl);
+    }
+
+    getById(id: number){
+        return this.http.get<Application>(`${this.apiUrl}/${id}`);
+    }
+
+    create(data: Partial<Application>){
+        return this.http.post<Application>(this.apiUrl, data);
+    }
+
+    update(id: number, data: Partial<Application>){
+        return this.http.put<Application>(`${this.apiUrl}/${id}`, data);
+    }
+
+    delete(id: number){
+        return this.http.delete<Application>(`${this.apiUrl}/${id}`);
+    }
+}
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { signal, Injectable } from "@angular/core";
+import { HttpClient, HttpBackend } from "@angular/common/http";
+import { Router } from "@angular/router";
+
+
+@Injectable({providedIn:"root"})
+export class AuthService{
+    private apiUrl = 'https://localhost:7194/api/auth';
+    private http: HttpClient;
+    currentUser = signal<string| null>(null);
+
+    constructor(private handler: HttpBackend, private router:Router){
+        this.http = new HttpClient(handler);
+        const token = localStorage.getItem('token');
+        if(token) this.currentUser.set(localStorage.getItem('fullName'))
+    }
+
+    register(data: {email:string, password: string, fullName: string}){
+        return this.http.post<{token:string, fullName: string, email:string}>(
+            `${this.apiUrl}/register`, data
+        )
+    };
+
+    login(data: {email:string, password: string}){
+        return this.http.post<{token:string, fullName: string, email:string}>(
+            `${this.apiUrl}/login`, data
+        )
+    };
+
+    saveToken(token: string, fullName:string){
+        localStorage.setItem('token', token);
+        localStorage.setItem('fullName', fullName);
+        this.currentUser.set(fullName);
+        this.router.navigate(['/applications'])
+    }
+
+    isLoggedIn(): boolean{
+        return !!localStorage.getItem('token');
+    }
+
+    logout() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('fullName');
+        this.currentUser.set(null);
+        this.router.navigate(['/login']);
+    }
+}
